3b7533404357045d263c9ea901f7fed7a8679bc6
8c5BO2VvipedhY <saravanasabesan94@gmail.com>
2026-05-19 13:45:02 -0400

    code pushing to branch

---
+++ README.md
@@ -0,0 +1,13 @@
+# AWS Serverless File Processing Pipeline
+
+This project demonstrates an event-driven AWS architecture using:
+
+- Amazon S3
+- AWS Lambda
+- Amazon DynamoDB
+- CloudFormation
+- CloudWatch
+
+Workflow:
+
+S3 Upload → Lambda Trigger → DynamoDB Storage
---
+++ lambda/lambda_function.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+import csv
+
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+TABLE_NAME = 'Students'
+
+def lambda_handler(event, context):
+
+    table = dynamodb.Table(TABLE_NAME)
+
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
+
+    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+
+    lines = response['Body'].read().decode('utf-8').splitlines()
+
+    csv_reader = csv.DictReader(lines)
+
+    for row in csv_reader:
+        table.put_item(
+            Item={
+                'studentId': row['studentId'],
+                'name': row['name'],
+                'department': row['department'],
+                'marks': row['marks']
+            }
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('CSV processed successfully!')
+    }
